a84f85529a3626ab0c0b6b8979cda6200aba053a
Max Baker <>
2003-02-27 21:37:41 +0000

    *** empty log message ***

---
+++ sql/users.sql
@@ -0,0 +1,12 @@
+-- $Id$
+
+DROP TABLE users;
+
+CREATE TABLE users (
+    username        varchar(50) PRIMARY KEY,
+    password        text,   -- md5 
+    creation        TIMESTAMP DEFAULT now(),
+    last_on         TIMESTAMP,
+    port_control    boolean,
+    admin           boolean
+                    );
