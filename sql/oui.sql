0de60637788b3ccbc138cefd9100a170c2ef2d25
cvs2svn <>
2006-12-01 14:24:20 +0000

    This commit was manufactured by cvs2svn to create tag 'pre_css_update'.
    Sprout from master 2006-12-01 14:24:19 UTC Bill Fenner 'Use $1 instead of $& to avoid triggering the $& performance penalty'
    Delete:
        ChangeLog
        INSTALL
        Makefile
        README
        UPGRADE
        bin/catalyst_mac_vlan.pl
        bin/debian_install.sh
        bin/doc_munge
        bin/fix_cvsroot
        bin/netdisco_daemon
        bin/parse_kismet_ap
        bin/parse_mac
        bin/pod_arg_link_fix
        bin/port_control
        bin/test_cache.pl
        bin/test_cdp.pl
        bin/test_dev.pl
        bin/test_fwtable.pl
        bin/test_iproute.pl
        bin/test_stats.pl
        doc/INSTALL.pod
        doc/README.pod
        doc/UPGRADE.pod
        netdisco
        netdisco-topology.txt
        netdisco.conf
        netdisco.crontab
        netdisco.pm
        netdisco_apache.conf
        netdisco_apache_dir.conf
        sql/admin.sql
        sql/device.sql
        sql/device_ip.sql
        sql/device_port.sql
        sql/device_port_log.sql
        sql/device_port_ssid.sql
        sql/log.sql
        sql/node.sql
        sql/node_ip.sql
        sql/node_nbt.sql
        sql/oui.sql
        sql/pg
        sql/sessions.sql
        sql/subnets.sql
        sql/upgrade-0.92-to-0.93.sql
        sql/upgrade-0.93-to-0.94.sql
        sql/upgrade-0.94-to-0.95.sql
        sql/user_log.sql
        sql/users.sql

--- sql/oui.sql
+++
@@ -1,9 +0,0 @@
--- $Id$
-
-DROP TABLE oui;
-
-CREATE TABLE oui (
-    oui         varchar(8) PRIMARY KEY,
-    company     text
-);
-
--- sql/sessions.sql
+++
@@ -1,9 +0,0 @@
--- $Id$
-
-DROP TABLE sessions;
-
-CREATE TABLE sessions (
-    id          char(32) NOT NULL PRIMARY KEY,
-    creation    TIMESTAMP DEFAULT now(),
-    a_session   text
-                       );
